7483b194626efc7c8ccd043ba2ca1722105a44b4
Antoine TAVANT <antoine.tavant@ecoco2.com>
2024-04-04 16:05:52 +0200

    update example

--- meteofrance_publicapi/raster.py
+++ meteofrance_publicapi/raster.py
@@ -18,28 +18,27 @@ def plot_tiff_file(filename, data_type="temperature"):
         This Function is more an "How-To" rather than a tool to use as is.
 
     """
-    # Open the file:
     data_field, transform = open_tiff_file(filename=filename)
-
     # Display the source image with cartopy using the geotransform from the source dataset
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
-
     im = ax.imshow(data_field,
                    cmap='jet',
                    extent=[transform[2],
                            transform[2] + transform[0]*data_field.shape[1],
                            transform[5] + transform[4]*data_field.shape[0],
                            transform[5]])
-
     ax.add_feature(feature.BORDERS.with_scale('10m'), color='black', linewidth=1)
     ax.add_feature(feature.COASTLINE.with_scale('10m'), color='black', linewidth=1)
-
     cbar = plt.colorbar(im, ax=ax, shrink=0.5)
-
-    lyon_coord = [4.8357, 45.7640]
-    paris_coord = [2.3522, 48.8566]
-
-    ax.plot(lyon_coord[0], lyon_coord[1], 'bo', transform=ccrs.PlateCarree())
-    ax.plot(paris_coord[0], paris_coord[1], 'bo', transform=ccrs.PlateCarree())
-
+    date = filename.stem.split("_")[-1]
+    if data_type == "temperature":
+        cbar.set_label("Temperature (°C)")
+        ax.set_title(f"Temperature at 2m above ground \n at {date} ")
+    add_city_location = False
+    if add_city_location:
+        lyon_coord = [4.8357, 45.7640]
+        paris_coord = [2.3522, 48.8566]
+        ax.plot(lyon_coord[0], lyon_coord[1], 'bo', transform=ccrs.PlateCarree())
+        ax.plot(paris_coord[0], paris_coord[1], 'bo', transform=ccrs.PlateCarree())
+    return ax
---
+++ meteofrance_publicapi/tests/test_import.py
@@ -0,0 +1,5 @@
+# test that it can be imported
+
+def test_import():
+    import meteofrance_publicapi
+    assert meteofrance_publicapi
